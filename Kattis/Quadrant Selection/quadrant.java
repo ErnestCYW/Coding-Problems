449001a46bbd3c89be64c2bb6a2a29a213a1594a
ErnestCYW <ernestcyw99@gmail.com>
2021-06-13 21:46:52 +0800

    solved quadrant

---
+++ Kattis/Quadrant Selection/quadrant.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Quadrant {
+
+    public static void main(String[] args) {
+        
+        Scanner io = new Scanner(System.in);
+        int x = io.nextInt();
+        int y = io.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else if (x > 0 && y < 0) {
+            System.out.println(4);
+        } 
+
+    }
+
+}
